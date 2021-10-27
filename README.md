c365d92d5313788e54ab3e2bb5ddf1e7f7bc6b0d
Jonathan <jonnybell123@hotmail.com>
2021-10-27 08:28:18 +0100

    fetch api working flawlessly, great job me

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ A form for searching pokemon by name
 
 populating a custom pokedex css card with data from:
 https://pokeapi.co/
+
+
+https://dribbble.com/shots/6540871-Pokedex-App
--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const fetchyGuy = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+const results = document.querySelector('#results');
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/pikachu"
+fetch(apiUrl)
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    const name = `<h2>${data.name}</h2>`
+    const id      = `<h2>${data.id}</h2>`
+    const picture = `<img src="${data.sprites.other['official-artwork'].front_default}" />`
+    results.insertAdjacentHTML('beforeend', name)
+    results.insertAdjacentHTML('beforeend', id)
+    results.insertAdjacentHTML('beforeend', picture)
+
+  });
+
+
+
+
+  // .then(response => response.json())
+  // .then(data => console.log(data));
+
+  // const name = data.name
+  // const id = data.id
+  // const image = data.sprites.official-artwork.front_default

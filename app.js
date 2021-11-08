39a0112f778d2e0159580ba72c3c0026f544d3fa
Jonathan <jonnybell123@hotmail.com>
2021-11-08 20:29:46 +0000

    finished pokemon app

--- app.js
+++ app.js
@@ -1,23 +1,65 @@
-const results = document.querySelector('#results');
-const apiUrl = "https://pokeapi.co/api/v2/pokemon/pikachu"
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    const name = `<h2>${data.name}</h2>`
-    const id      = `<h2>${data.id}</h2>`
-    const picture = `<img src="${data.sprites.other['official-artwork'].front_default}" />`
-    results.insertAdjacentHTML('beforeend', name)
-    results.insertAdjacentHTML('beforeend', id)
-    results.insertAdjacentHTML('beforeend', picture)
+const poke_container = document.getElementById('poke-container');
+const pokemon_number = 150;
+const colors = {
+  fire: '#FDDFDF',
+  grass: '#DEFDE0',
+  electric: '#FCF7DE',
+  water: '#DEF3FD',
+  ground: '#f4e7da',
+  rock: '#d5d5d4',
+  fairy: '#fceaff',
+  poison: '#98d7a5',
+  bug: '#f8d5a3',
+  dragon: '#97b3e6',
+  psychic: '#eaeda1',
+  flying: '#F5F5F5',
+  fighting: '#E6E0D4',
+  normal: '#F5F5F5'
+};
 
-  });
+const main_types = Object.keys(colors);
+
+const fetchPokemons = async () => {
+  for (let i=1; i <= pokemon_number; i++) {
+    await getPokemon(i);
+  }
+};
+
+const getPokemon = async id => {
+  const url =
+`https://pokeapi.co/api/v2/pokemon/${id}`;
+
+  const res = await fetch(url);
+  const pokemon = await res.json();
+  createPokemonCard(pokemon);
+};
+
+capitalize = (string) => {return string[0].toUpperCase() + string.slice(1)};
+
+ createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement('div');
+  pokemonEl.classList.add('pokemon');
+  const poke_types = pokemon.types.map(el => el.type.name);
+  const type = main_types.find(type => poke_types.indexOf(type) > -1);
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const color = colors[type];
+  pokemonEl.style.backgroundColor = color;
+  const pokeInnerHTML =
+  `
+  <div class="image-container">
+    <img src="${pokemon.sprites.other['official-artwork'].front_default}">
+  </div>
+  <div class="info">
+    <span class="number">#${pokemon.id.toString().padStart(3, '0')}</span>
+    <h3 class="name">${name}</h3>
+    <small class="type">Type: <span>${capitalize(type)}</span></small>
+  </div>
+</div>
+  `;
+  pokemonEl.innerHTML = pokeInnerHTML;
+  poke_container.appendChild(pokemonEl);
+};
 
 
-
-
-  // .then(response => response.json())
-  // .then(data => console.log(data));
-
-  // const name = data.name
-  // const id = data.id
-  // const image = data.sprites.official-artwork.front_default
+fetchPokemons();
+getPokemon();
